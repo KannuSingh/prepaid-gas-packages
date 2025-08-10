5b47a75be2cf99f49a9a9c9f0cce1d4539c86493
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-08-10 01:30:14 -0400

    feat(constants): add bundle analysis tooling and cleanup

--- packages/constants/test-consumer.ts
+++
@@ -1,5 +0,0 @@
-// Test consumer that only imports the optimized ABI
-import { ONE_TIME_USE_PAYMASTER_ABI_OPTIMIZED, BASE_PAYMASTER_ABI } from '../src/abi';
-
-console.log('OneTimeUse ABI length:', ONE_TIME_USE_PAYMASTER_ABI_OPTIMIZED.length);
-console.log('Base ABI length:', BASE_PAYMASTER_ABI.length);
